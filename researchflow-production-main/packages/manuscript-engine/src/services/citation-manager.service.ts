a8771538ac9cd2c7ee0d97bc67f4dc5c99ea9751
ry86pkqf74-rgb <Logan.glosser@gmail.com>
2026-02-10 02:10:21 -0500

    Align citation style keys to enum

--- researchflow-production-main/packages/manuscript-engine/src/services/citation-manager.service.ts
+++ researchflow-production-main/packages/manuscript-engine/src/services/citation-manager.service.ts
@@ -57,10 +57,11 @@ export class CitationManagerService {
   formatCitation(citation: Citation, style: CitationStyle): Record<CitationStyle, string> {
     return {
       superscript_number: this.formatAMA(citation),
-      APA: this.formatAPA(citation),
-      Vancouver: this.formatVancouver(citation),
-      NLM: this.formatNLM(citation),
-      Chicago: this.formatChicago(citation),
+      author_year: this.formatAPA(citation),
+      author_year_full: this.formatAPA(citation),
+      number_parentheses: this.formatVancouver(citation),
+      footnote: this.formatNLM(citation),
+      endnote: this.formatChicago(citation),
     };
   }
 
